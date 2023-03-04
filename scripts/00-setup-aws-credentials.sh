24ea3757a17c20afac080eb81f6fc7a68ecc32fd
José Alberto Flores <josefloressv@gmail.com>
2023-03-04 14:47:38 +0000

    udate setup aws credentials snippet

--- scripts/00-setup-aws-credentials.sh
+++ scripts/00-setup-aws-credentials.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 # https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-envvars.html
-export AWS_ACCESS_KEY_ID=AKIAZRTXMEQLBPBFPMPU
-export AWS_SECRET_ACCESS_KEY=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
+export AWS_ACCESS_KEY_ID=changeme
+export AWS_SECRET_ACCESS_KEY=changeme
 export AWS_DEFAULT_REGION=us-east-1
+
+# validate
+aws sts get-caller-identity
+aws iam get-user
