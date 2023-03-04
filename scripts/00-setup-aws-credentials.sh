87faca16e585b3ebcdff6a5e4d550ed8cc2c95a8
José Alberto Flores <josefloressv@gmail.com>
2023-03-04 14:45:34 +0000

    Update setup aws credentials snippet

--- scripts/00-setup-aws-credentials.sh
+++ scripts/00-setup-aws-credentials.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-# From A Cloud Guru
-export AWS_ACCESS_KEY_ID=anaccesskey
-export AWS_SECRET_ACCESS_KEY=asecretkey
+# https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-envvars.html
+export AWS_ACCESS_KEY_ID=AKIAZRTXMEQLBPBFPMPU
+export AWS_SECRET_ACCESS_KEY=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
+export AWS_DEFAULT_REGION=us-east-1
